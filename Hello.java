d01611f086a0ac382788968d11f9a0cf272736d8
yuefengliarizona <yuefengli@email.arizona.edu>
2017-09-11 16:56:16 -0700

    jhkjh

--- Hello.java
+++ Hello.java
@@ -8,7 +8,10 @@ public class Hello {
 
     }
 
-    public static void callSign() {
+    public static void callSign() {System.out.
+println("Lock S-foils in attack position."
+    			);
+    	
         
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
@@ -30,3 +33,5 @@ public class Hello {
     }
 
 }
+
+
